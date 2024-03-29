9266cf1779bdc3d063d084d8d84d5c52b0d1c1f3
Tagert <mini.andrius@gmail.com>
2024-03-29 15:02:28 +0200

    changed: get, post, put, delete to use mongoose

--- src/controllers/flights.js
+++ src/controllers/flights.js
@@ -1,7 +1,5 @@
 import { FlightModel } from "../models/flights.js";
 
-let flightsList = [];
-
 const ADD_FLIGHT = async (req, res) => {
   try {
     const flight = new FlightModel({
@@ -36,74 +34,86 @@ const GET_ALL_FLIGHTS = async (req, res) => {
   }
 };
 
-const GET_FLIGHT_BY_ID = (req, res) => {
-  const findFlight = flightsList.find((f) => f.id === req.params.id);
+const GET_FLIGHT_BY_ID = async (req, res) => {
+  try {
+    const findFlight = await FlightModel.findById(req.params.id);
 
-  if (!findFlight) {
-    return res.status(400).json({
-      message: `The entered ID (${req.params.id}) does not exist. Please try entering a different ID.`,
+    if (!findFlight) {
+      return res.status(400).json({
+        message: `The entered ID (${req.params.id}) does not exist. Please try entering a different ID.`,
+      });
+    }
+
+    return res.json(findFlight);
+  } catch (err) {
+    console.log("HANDLED ERROR:", err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+const DELETE_FLIGHT_BY_ID = async (req, res) => {
+  try {
+    const deleteFlight = await FlightModel.findByIdAndDelete(req.params.id);
+
+    if (!deleteFlight) {
+      return res
+        .status(404)
+        .json({ message: `Flight with ID (${req.params.id}) was not found` });
+    }
+
+    return res.status(200).json({
+      message: `Flight with ID (${req.params.id}) was deleted`,
+      flight: deleteFlight,
     });
+  } catch (err) {
+    console.log("HANDLED ERROR:", err);
+    return res.status(500).json({ error: "Something went wrong" });
   }
-
-  return res.json(findFlight);
 };
 
-const DELETE_FLIGHT_BY_ID = (req, res) => {
-  const remainingFlight = flightsList.filter((f) => {
-    return req.params.id !== f.id;
-  });
-
-  const isFlightExists = flightsList.some((f) => f.id === req.params.id);
-
-  if (!isFlightExists) {
-    return res
-      .status(404)
-      .json({ message: `Flight with ID (${req.params.id}) was not found` });
-  }
-
-  flightsList = remainingFlight;
-
-  return res
-    .status(200)
-    .json({ message: `Flight with ID (${req.params.id}) was deleted` });
-};
-
-const UPDATE_FLIGHT_BY_ID = (req, res) => {
-  const isFlightExists = flightsList.some((f) => f.id === req.params.id);
-
-  if (!isFlightExists) {
-    return res
-      .status(404)
-      .json({ message: `Flight with ID (${req.params.id}) was not found` });
-  }
-
-  const index = flightsList.findIndex((f) => f.id === req.params.id);
-
-  flightsList[index] = { ...flightsList[index], ...req.body };
-
-  return res.json({ updatedFlight: flightsList[index] });
-};
-
-const PAGINATED_FLIGHTS = (req, res) => {
-  const pageNumber = Number(req.params.pageNumber);
-  const pageSize = 5;
-
-  if (!flightsList.length) {
-    return res.status(200).json({ message: "Data not exist" });
-  }
-
-  if (typeof pageNumber === "number" && pageNumber > 0) {
-    const startingPoint = (pageNumber - 1) * pageSize;
-    const paginatedFlights = flightsList.slice(
-      startingPoint,
-      startingPoint + pageSize
+const UPDATE_FLIGHT_BY_ID = async (req, res) => {
+  try {
+    const updateFlight = await FlightModel.findByIdAndUpdate(
+      { _id: req.params.id },
+      { ...req.body },
+      { new: true }
     );
 
-    return res.json({ flights: paginatedFlights });
-  } else {
-    return res.json({
-      error: `Flights list page number (${pageNumber}) is incorrect, it must be a number and not a 0`,
-    });
+    if (!updateFlight) {
+      return res
+        .status(404)
+        .json({ message: `Flight with ID (${req.params.id}) was not found` });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "updated", updatedFlight: updateFlight });
+  } catch (err) {
+    console.log("HANDLED ERROR:", err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+const PAGINATED_FLIGHTS = async (req, res) => {
+  try {
+    const pageNumber = Number(req.params.pageNumber);
+    const pageSize = 3;
+
+    if (!isNaN(pageNumber) && pageNumber > 0) {
+      const paginatedFlights = await FlightModel.find()
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+        .exec();
+
+      return res.json({ flights: paginatedFlights });
+    } else {
+      return res.json({
+        error: `Flights list page number (${pageNumber}) is incorrect, please use a number which is greater than zero`,
+      });
+    }
+  } catch (err) {
+    console.log("HANDLED ERROR:", err);
+    return res.status(500).json({ error: "Something went wrong" });
   }
 };
 
